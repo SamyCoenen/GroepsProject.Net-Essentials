ce3f63c77f3aa4a893999f0caef95abfb4a2aff4
cabrozz <marnicbroux@hotmail.com>
2015-05-08 12:08:15 +0200

    Marnic

---
+++ Wiskunde/Wiskunde.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace leren
+{
+    /// <summary>
+    /// Interaction logic for Wiskunde.xaml
+    /// </summary>
+    public partial class Wiskunde : Window
+    {
+        private int graad;
+        private List<KeuzeVraag> keuzevragen = new List<KeuzeVraag>();
+        private List<int> vragenGeschiedenis = new List<int>();
+        private List<KeuzeAntwoord> keuzeAntwoorden = new List<KeuzeAntwoord>();
+        private int index;
+        public Wiskunde()
+        {
+            InitializeComponent();
+            Loaded += Wiskunde_Loaded;
+
+        }
+        private void Wiskunde_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (graad == 0)
+            {
+                Makkelijk();
+            }
+            else
+            {
+                IODatabase database = new IODatabase("wiskunde");
+                keuzevragen = database.ReadKeuzeVragenByGraad(graad);
+                Moeilijk();
+                moeilijk.Visibility = Visibility.Visible;
+                makkelijk.Visibility = Visibility.Hidden;
+            }
+        }
+        public void Makkelijk()
+        {
+            int Min = 0;
+            int Max = 20;
+
+          
+            int[] getal = new int[10];
+            string[] symbool = new string[2]{"+", "-"};
+
+            Random randNum = new Random();
+            for (int i = 0; i < getal.Length; i++)
+            {
+                getal[i] = randNum.Next(Min, Max);
+            }
+            vraag1.Content = (getal[0] + " " + symbool[0] + " " + getal[1]);
+            vraag2.Content = (getal[2] + " " + symbool[1] + " " + getal[3]);
+            vraag3.Content = (getal[4] + " " + symbool[0] + " " + getal[5]);
+            vraag4.Content = (getal[6] + " " + symbool[1] + " " + getal[7]);
+            vraag5.Content = (getal[8] + " " + symbool[0] + " " + getal[9]);
+
+        }
+        public void Moeilijk()
+        {
+            index = 0;
+            Random rnd = new Random();
+            do
+            {
+                index = rnd.Next(0, keuzevragen.Count - 1);
+            } while (vragenGeschiedenis.Contains(index));
+            vragenGeschiedenis.Add(index);
+
+            KeuzeVraag keuzevraag = keuzevragen[index];
+            lblVraag.Content = keuzevraag.Vraag;
+
+            radioBtnGrid.Children.Clear();
+            radioBtnGrid.RowDefinitions.Clear();
+
+            for (int i = 0; i < keuzevraag.Keuzes.Count; i++)
+            {
+                RadioButton antwoordbtn = new RadioButton();
+                antwoordbtn.Tag = i;
+                antwoordbtn.Content = keuzevraag.Keuzes[i];
+                RowDefinition row = new RowDefinition();
+                row.Height = new GridLength(45);
+                radioBtnGrid.RowDefinitions.Add(row);
+                Grid.SetRow(antwoordbtn, i);
+                radioBtnGrid.Children.Add(antwoordbtn);
+            }
+        }
+        public int Graad
+        {
+            get { return graad; }
+            set { graad = value; }
+        }
+    }
+
+}
---
+++ Wiskunde/WiskundeMenu.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace leren
+{
+    /// <summary>
+    /// Interaction logic for WiskundeMenu.xaml
+    /// </summary>
+    public partial class WiskundeMenu : Window
+    {
+        public WiskundeMenu()
+        {
+            InitializeComponent();
+
+        }
+        // Wiskunde oefeningen openen
+        private void Image_MouseDown(object sender, RoutedEventArgs e)
+        {
+            int graad = (int)graadSlider.Value;
+            Wiskunde wiskundeWindow = new Wiskunde();
+            wiskundeWindow.Graad = (int)graadSlider.Value;
+            wiskundeWindow.Show();
+        }
+    }
+}
